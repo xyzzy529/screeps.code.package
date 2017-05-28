1ef0d594319b2a111d3985662fa53a5bc3bd4503
cyberblast <git@cyberblast.org>
2017-05-28 10:04:32 +0200

    OCS code

---
+++ README.md
@@ -0,0 +1,112 @@
+## Getting started with the repository  
+
+### Prerequisites 
+
+* On Unix/Linux Distrubutions  
+  You must have SSH keys linked to your GitHub account before following these instrucitons.  See https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account for details on setting this up.  
+  
+* On Windows  
+  If you are not familiar in using a CLI (command line interface), it is recommened that you use Github Desktop https://desktop.github.com/, it is quite simple.  
+  If you don't want to use Github Desktop, you must have SSH keys linked to your GitHub account before following these instructions (at least for *Installation: 3. Clone repository and submodules*).  
+  
+  To use **powershell**, you may have to do the following steps: 
+  * Install OpenSSH  
+    https://github.com/PowerShell/Win32-OpenSSH/wiki/Install-Win32-OpenSSH 
+  * Install Git  
+    https://git-scm.com/download/win
+  * Install posh-git  
+    https://github.com/dahlbyk/posh-git/blob/master/README.md
+  * Extend PATH variable
+    * Press "windows key" + "pause/break"
+    * Click `Advanced system settings`
+    * Click `Environment Variables`
+    * In the `System Variables` area, locate the `Path` variable, highlight it and click `Edit`
+    * Add path variables for git and OpenSSH (depending on where you put them)
+      * C:\Program Files\Git
+      * C:\Program Files\Git\bin
+      * C:\Program Files\Git\cmd
+      * C:\Program Files\OpenSSH
+      
+    
+  * Add SSH keys to your github account  
+    https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account
+
+  Alternatively, you can use the git bash (included in git) or git shell (included in Github Desktop) or maybe your IDE or ...
+
+### Installation
+
+1. Install Node  
+  https://nodejs.org/en/
+
+2. Install the grunt CLI (using admin rights/sudo if required)  
+  `npm install -g grunt-cli`  
+
+3. Clone repository and submodules  
+  * via CLI  
+    `git clone --recursive https://github.com/ScreepsOCS/screeps.code.package.git`  
+  * via Github Desktop  
+    Click the plus sign (+) at the top left corner, click clone and select `ScreepsOCS`, then `screeps.code.package`.  
+
+4. Install dependencies after changing directory into the newly cloned work area  
+  `cd screeps.code.package`  
+  `npm i`
+
+5. Create a screeps.json file (copy example.screeps.json) & enter screeps account login data  
+  `cp example.screeps.json screeps.json`
+
+### Usage
+
+1. Commands
+  * to build (without deployment)  
+  `grunt`  
+  * to build & deploy  
+  `grunt deploy [--branch=<customBranch>]`
+  * to build & deploy automatically upon saving  
+  `grunt watch [--branch=<customBranch>]`
+  * to build & publish to a folder (define in screeps.json)  
+  `grunt publish`
+  
+2. After your first deployment using this repository, please call `delete Memory.modules;` from within the screeps console, to update module references.  
+  *required only once, or maybe upon changes regarding module loading*  
+
+3. You may want to create a directory called `overrides`. Here you'll be able to place `custom.*` and `viral.*` files that will be merged eventually.  
+Please note, that it is required to call `getPath('<originalModuleNameWithoutExtension>', true)`, when adding a new `custom.*` or `viral.*` file to bust the cache and enable it.  
+
+4. Deploy Options
+  * `--branch` : Use with `grunt deploy` or `grunt watch` to deploy code to a custom screeps branch. Will deploy to branch defined in screeps.json when not specifying --branch on command.
+
+## Advanced Branch Management
+
+### Reintegrate Documentation
+
+* Create feature mixin branch.
+
+`grunt reintegrate:_BRANCH_[:clean]`
+
+Creates _\_BRANCH\__ from reintegrate.json:_$target_.reset and merges all branches in reintegrate.json:_$target_.merge
+
+* Fetch all submodules
+
+`grunt gitfetch`
+
+Runs git fetch on `screeps.code`, `screeps.code.extension`, and `overries` (if overrides contains `.git`).
+
+### Reintegrate Guide
+
+If you find that you want to try multiple feature branches you can use the `reintegrate` feature to automatically merge them into your code.
+
+1. Make sure that all of your virals and personalizations are located in the `overrides/` directory or project root.
+
+2. Modify `/reintegrate.json` or create a copy in your `overrides/` directory. Change the `"reset"` property to indicate
+the branch which you are merging features into (usually `dev`). Add feature branches into the `"merge"` property (make sure
+to include the remote to keep this tidy, e.g. `origin/my-feature-branch`)
+
+3. Decide on a branch name for your integration work. The example used here is `integrationBranch`
+
+4. Run the grunt tasks to update and reintegrate the features you want to use:
+
+`grunt gitfetch reintegrate:integrationBranch`
+
+5. If anything fails try removing feature branches to find a clean mixture. Experienced git users can make use of `git config rerere.enabled true` and manually resolve merge conflicts.
+
+6. To test the current `dev` branches, simply run `grunt gitfetch reintegrate:integrationBranch:clean`
---
+++ gruntfile.js
@@ -0,0 +1,250 @@
+'use strict';
+const optional = require('require-optional');
+
+module.exports = function(grunt) {
+    const config = require('./screeps.json');
+    if(!config.branch) {
+        config.branch = 'sim';
+    }
+    if(!config.ptr) {
+        config.ptr = false;
+    }
+    if(!config.publishDir) {
+        config.publishDir = 'pub/';
+    }
+
+    require('load-grunt-tasks')(grunt);
+
+    const reintegrate = optional('./overrides/reintegrate.json')
+        || optional('./reintegrate.json');
+
+    // Override branch in screeps.json
+    // grunt deploy --branch=<customBranch>
+    var branch = false;
+    if(grunt.option('branch')) branch = grunt.option('branch');
+
+    const submodules = ['screeps.code.extension', 'screeps.code'];
+    if (grunt.file.exists('./overrides/.git')) submodules.push('overrides');
+
+    const gitfetch = {};
+    submodules.forEach(function(subdir) {
+        gitfetch[subdir] = {
+            options: {
+                cwd: subdir,
+                all: true,
+            },
+        };
+    });
+
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: config.email,
+                password: config.password,
+                branch: branch ? branch : config.branch,
+                ptr: config.ptr
+            },
+            dist: {
+                src: ['dist/*.js']
+            }
+        },
+        watch: {
+            scripts: {
+                files: [
+                    'screeps.code/*.js',
+                    'screeps.code.extension/*.js',
+                    'overrides/*.js'
+                ],
+                tasks: ['deploy'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+        clean: ['dist/','pack/'],
+        copy: {
+            public: {
+                files: [{
+                    expand: true,
+                    cwd: 'screeps.code/',
+                    src: '**',
+                    dest: 'dist/',
+                    filter: 'isFile',
+                    rename: function (dest, src) {
+                        // Change the path name. utilize dots for folders
+                        return dest + src.replace(/\//g,'.');
+                    }
+                }]
+            },
+            internal: {
+                files: [{
+                    expand: true,
+                    cwd: 'screeps.code.extension/',
+                    src: '**',
+                    dest: 'dist/',
+                    filter: 'isFile',
+                    rename: function (dest, src) {
+                        // Change the path name. utilize dots for folders
+                        return dest + src.replace(/\//g,'.');
+                    }
+                }]
+            },
+            overrides: {
+                files: [{
+                    expand: true,
+                    cwd: 'overrides/',
+                    src: '**',
+                    dest: 'dist/',
+                    filter: 'isFile',
+                    rename: function (dest, src) {
+                        // Change the path name. utilize dots for folders
+                        return dest + src.replace(/\//g,'.');
+                    }
+                }]
+            },
+            publish: {
+                files: [{
+                    expand: true,
+                    cwd: 'dist/',
+                    src: '**',
+                    dest: config.publishDir,
+                    filter: 'isFile',
+                    rename: function (dest, src) {
+                        // Change the path name. utilize dots for folders
+                        return dest + src.replace(/\//g,'.');
+                    }
+                }]
+            },
+        },
+        webpack: {
+            main: {
+                entry: './dist/main.js',
+                output: {
+                    path: 'pack/',
+                    filename: 'main.js',
+                    libraryTarget: 'commonjs2'
+                },
+                module: {
+                    loaders: [{
+                        test: /\.js$/,
+                        exclude: /(src|node_modules|ScreepsAutocomplete)/,
+                        loader: 'babel-loader',
+                        query: {
+                            presets: [
+                                require.resolve('babel-preset-es2015')
+                            ]
+                        }
+                    }]
+                }
+            }
+        },
+        uglify: {
+            my_target: {
+                files: [{
+                    expand: true,
+                    cwd: 'pack',
+                    src: 'main.js',
+                    dest: 'pack'
+                }]
+            }
+        },
+        reintegrate: {
+            options: reintegrate,
+        },
+        gitfetch: gitfetch,
+    });
+    // 
+    grunt.registerTask('switch-to-pack-deploy', function () {
+        grunt.config.set('screeps.dist.src', ['pack/main.js']);
+    });
+    // clean deployment (dry run)
+    grunt.registerTask('default', ['clean', 'copy:public', 'copy:internal', 'copy:overrides']);
+    // clean deployment
+    grunt.registerTask('deploy', ['clean', 'copy:public', 'copy:internal', 'copy:overrides', 'screeps']);
+    // clean deployment to directory
+    grunt.registerTask('publish', ['clean', 'copy:public', 'copy:internal', 'copy:overrides', 'copy:publish']);
+    // clean deployment (public only)
+    grunt.registerTask('public-deploy', ['clean', 'copy:public', 'screeps']);
+    // single file [experimental] (dry run)
+    grunt.registerTask('compress', ['clean', 'copy:public', 'copy:internal', 'copy:overrides', 'webpack']);
+    // single file [experimental]
+    grunt.registerTask('compress-deploy', ['clean', 'copy:public', 'copy:internal', 'copy:overrides', 'webpack', 'switch-to-pack-deploy','screeps']);
+    // uglified [experimental] (dry run)
+    grunt.registerTask('ugly', ['clean', 'copy:public', 'copy:internal', 'copy:overrides', 'webpack', 'uglify']);
+    // uglified [experimental]
+    grunt.registerTask('ugly-deploy', ['clean', 'copy:public', 'copy:internal', 'copy:overrides', 'webpack', 'uglify', 'switch-to-pack-deploy', 'screeps']);
+    grunt.registerTask('reintegrate', 'Create a new integration branch with branches configured from reintegrate.json', function(branch, targetOption) {
+        const options = this.options();
+        if (Object.getOwnPropertyNames(options).length === 0) {
+            grunt.fail.fatal("reintegrate requires external config: reintegrate.json");
+            return false;
+        }
+
+        const optionOutput = {
+            gitadd: {},
+            gitcommit: {},
+            gitcheckout: {},
+            gitreset: {},
+            gitmerge: {},
+        };
+
+        let runMerge = false;
+        for (const subdir in options) {
+            optionOutput.gitadd[subdir] = {
+                options: {
+                    cwd: subdir,
+                    all: true,
+                }
+            };
+            optionOutput.gitcommit[subdir] = {
+                options: {
+                    cwd: subdir,
+                    message: 'reintegrate ' + subdir + ' before branching to ' + branch,
+                    allowEmpty: true,
+                }
+            };
+            optionOutput.gitcheckout[subdir] = {
+                options: {
+                    cwd: subdir,
+                    branch: branch,
+                    overwrite: true,
+                }
+            };
+            optionOutput.gitreset[subdir] = {
+                options: {
+                    cwd: subdir,
+                    mode: 'hard',
+                    commit: options[subdir].reset,
+                }
+            };
+
+            if (targetOption !== "clean" && options[subdir].merge) {
+                for (const merge of options[subdir].merge) {
+                    runMerge = true;
+                    const key = subdir + "-" + merge;
+                    optionOutput.gitmerge[key] = {
+                        options: {
+                            cwd: subdir,
+                            branch: merge,
+                        }
+                    }
+                }
+            }
+        }
+
+        for (const task in optionOutput) {
+            grunt.config(task, optionOutput[task]);
+        }
+
+        grunt.task.run([
+            'gitadd', // add loose files
+            'gitcommit', // commit changes
+            'gitcheckout', // create new branch
+            'gitreset', // reset hard to base branch
+        ]);
+
+        if (runMerge) {
+            grunt.task.run(['gitmerge']); // merge features
+        }
+    });
+};
